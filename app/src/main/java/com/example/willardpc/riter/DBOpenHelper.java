7632a2b2fbaa072108c2982fb399b82331aca569
willardshikami <willardshikami@gmail.com>
2016-09-19 07:54:43 +0300

    adding DB

--- app/src/main/java/com/example/willardpc/riter/DBOpenHelper.java
+++ app/src/main/java/com/example/willardpc/riter/DBOpenHelper.java
@@ -1,47 +1,40 @@
 package com.example.willardpc.riter;
 
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.StrictMode;
-import android.view.View;
 
 public class DBOpenHelper extends SQLiteOpenHelper{
 
-    //Constance fot DB name and version
+    //Constants for db name and version
     private static final String DATABASE_NAME = "notes.db";
     private static final int DATABASE_VERSION = 1;
 
-    //Constance for identifying tables and columns
+    //Constants for identifying table and columns
     public static final String TABLE_NOTES = "notes";
-    public static final String NOTE_ID = "id";
+    public static final String NOTE_ID = "_id";
     public static final String NOTE_TEXT = "noteText";
     public static final String NOTE_CREATED = "noteCreated";
 
-    public static final String [] ALL_COLUMNS =
-            {NOTE_ID, NOTE_TEXT, NOTE_CREATED};
-
     //SQL to create table
     private static final String TABLE_CREATE =
-            "CREATE TABLE " + TABLE_NOTES + " ( " +
+            "CREATE TABLE " + TABLE_NOTES + " (" +
                     NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    NOTE_TEXT + " TEXT " +
-                    NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP " +
+                    NOTE_TEXT + " TEXT, " +
+                    NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP" +
                     ")";
 
-    public DBOpenHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLE_CREATE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
-        onCreate(db);
+
     }
 }
