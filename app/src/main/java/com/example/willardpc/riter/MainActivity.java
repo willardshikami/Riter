66773621025acff5738f950b87802c94211a5d65
willardshikami <willardshikami@gmail.com>
2016-09-25 17:41:27 +0300

    updated db

--- app/src/main/java/com/example/willardpc/riter/MainActivity.java
+++ app/src/main/java/com/example/willardpc/riter/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.willardpc.riter;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         insertNote("New note");
 
+        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+
+        String from = (DBOpenHelper.NOTE_TEXT);
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter
+
+
     }
+
 
     private void insertNote(String noteText) {
         ContentValues values = new ContentValues();
@@ -25,3 +38,4 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", "Inserted note" + noteUri.getLastPathSegment());
     }
 }
+
